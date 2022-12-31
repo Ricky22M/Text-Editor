ecbe517c7f3c7ed108a728383a0467b08ad7e61a
Ricky Esparza <RickyEsparza@Rickys-MacBook.local>
2022-12-30 19:52:23 -0700

    finished TODOs in src/js folder

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,15 +13,32 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.error('putDb not implemented');
 
-const contactDB = await openDB('JATE', 1);
+  const jateDB = await openDB('jate', 1);
 
-const tx = contactDb.transaction('JATE', 'readwrite');
+  const tx = jateDB.transaction('jate', 'readwrite');
 
-const store = tx.objectStore('JATE');
+  const store = tx.objectStore('jate');
 
+  const request = store.put({ id: 1, value: content });
+
+  const result = await request;
+  console.log(result);
+}
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.error('getDb not implemented');
+
+  const jateDB = await openDB('jate', 1);
+
+  const request = store.getAll();
+
+  const result = await request;
+
+  console.log(result?.value);
+  return result?.value;
+}
 
 initdb();
